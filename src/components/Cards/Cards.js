9dcf6d70b991706130b4b517d82bb3fe178623ce
Iann-rst <iannrst11@gmail.com>
2021-08-21 22:39:26 -0300

    criação do componente card

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,12 @@
+function Cards(){
+  return(
+    <div class="card">
+      <h5 class="card-header">Featured</h5>
+      <div class="card-body">
+        <p>Meu texto no card</p>
+      </div>
+    </div>
+  )
+}
+
+export default Cards;
